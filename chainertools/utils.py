a9e1f60ba151c2c6f7c9a2445ed08a5339585c62
Ronan Le Hy <ronan.lehy@gmail.com>
2018-12-21 21:24:11 +0100

    Fix tar downloading and unpacking.

--- chainertools/utils.py
+++ chainertools/utils.py
@@ -1,10 +1,10 @@
 import os
 import re
 import errno
-import requests
-import tarfile
 import hashlib
 import logging
+import requests
+import subprocess
 from tqdm import tqdm
 
 log = logging.getLogger(__name__)
@@ -43,6 +43,9 @@ def mkdir_p(path):
             raise
 
 
+CHUNK_SIZE = 32768
+
+
 # https://stackoverflow.com/questions/16694907/how-to-download-large-file-in-python-with-requests-py
 def download_file(url, local_filename):
     if 'drive.google.com' in url:
@@ -50,7 +53,7 @@ def download_file(url, local_filename):
         return download_file_from_google_drive(file_id, local_filename)
     with open(local_filename, 'wb') as f:
         with requests.get(url, stream=True) as r:
-            for chunk in r.iter_content(chunk_size=1024):
+            for chunk in tqdm(r.iter_content(chunk_size=CHUNK_SIZE)):
                 f.write(chunk)
 
 
@@ -63,13 +66,14 @@ def download_file_from_google_drive(file_id, destination):
 
         return None
 
-    def save_response_content(response, destination):
-        CHUNK_SIZE = 32768
-
+    def save_response_content(response, destination, file_size):
         with open(destination, "wb") as f:
-            for chunk in tqdm(response.iter_content(CHUNK_SIZE)):
-                if chunk:  # filter out keep-alive new chunks
+            with tqdm(
+                    unit='B', unit_scale=True, unit_divisor=1024,
+                    total=file_size) as progress:
+                for chunk in response.iter_content(CHUNK_SIZE):
                     f.write(chunk)
+                    progress.update(CHUNK_SIZE)
 
     url = "https://docs.google.com/uc?export=download"
 
@@ -78,11 +82,31 @@ def download_file_from_google_drive(file_id, destination):
     response = session.get(url, params={'id': file_id}, stream=True)
     token = get_confirm_token(response)
 
+    file_size = -1
     if token:
         params = {'id': file_id, 'confirm': token}
-        response = session.get(url, params=params, stream=True)
 
-    save_response_content(response, destination)
+        # The returned header does not include content-length.
+        # headers = session.head(url, params=params).headers
+        # log.info('headers: %s', headers)
+        # file_size = int(headers["Content-Length"])
+        # log.info("file size: %d", file_size)
+
+        response = session.get(url, params=params, stream=True)
+        # There must be a way to get the content-length, but it eludes
+        # me atm.
+        # log.info(response.headers)
+
+    save_response_content(response, destination, file_size)
+
+
+def run(command):
+    with subprocess.Popen(
+            command, stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT) as popen:
+        with tqdm(unit='files') as progress:
+            for line in iter(popen.stdout.readline, b''):
+                progress.update(1)
 
 
 def download_tar(url, destination):
@@ -99,5 +123,14 @@ def download_tar(url, destination):
         os.rename(temp_tar_file_name, tar_file_name)
 
     log.info("extracting %s to directory %s", tar_file_name, destination)
-    with tarfile.open(tar_file_name) as tar:
-        tar.extractall(path=destination)
+    # tarfile is terribly slow and memory-hungry on a big archive
+    # with tarfile.open(tar_file_name) as tar:
+    #    tar.extractall(path=destination)
+    # tar_ret = subprocess.call([
+
+    run([
+        "tar", "--directory", destination, "--verbose", "--totals", "-x", "-f",
+        tar_file_name
+    ])
+    if tar_ret != 0:
+        raise RuntimeError("tar returned non-zero: {}".format(tar_ret))
